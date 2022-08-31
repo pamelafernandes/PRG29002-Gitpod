0ff33114a65c6d332b663180585568e1fbd8a9a0
pamelafernandes <pamela.f16@aluno.ifsc.edu.br>
2022-08-31 12:06:19 +0000

    aula4

---
+++ aula4-2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+// expressãologica
+int main (void)
+{
+    int a, b,c ;
+a=10;
+b=20;
+c= (a ==b);
+printf ("%d %d %d \n", a, b, c);
+    
+
+    // Booleanos
+    int p, q;
+    printf ("Entre com os valores de p e q");
+    scanf("%d %D", &p,&q);
+    printf("p e q ; %d\n, p && q");
+    printf (" p ou q : %d\n", p ||q);
+    printf ("Não p: %d\n", !p);
+    printf (" NÃO q: %d\n",!q);
+
+}return 0;return 0;
---
+++ aula4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main (void)
+{
+  int b =1357;
+
+  printf ("%d", b);
+  b /=10; // mesmo que b = b /10
+  printf ("%d", b);
+  b/=10;
+  printf("%d", b);
+  b/=10;
+  printf("%d\n", b);
+  
+       return 0;
+
+
+
+
+
+}
